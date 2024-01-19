c7e278affad799e46dbdb7651b39a8655a9a09cd
Keenan Shumard <keenan.shumard@gmail.com>
2024-01-18 20:09:59 -0600

    Direct fixes to get ready for first deploy of new version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,17 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
 function Footer() {
     return (
-        <Typography justifySelf="flex-end">
-            This tool was created to be used with the game found here:
-            http://www.keeptalkinggame.com/
-        </Typography>
+        <Grid item container justifySelf="flex-end" direction="column">
+            <Grid item component={Typography}>
+                Bomb Manual v{process.env.REACT_APP_VERSION}
+            </Grid>
+            <Grid item component={Typography}>
+                This tool was created to be used with the game found here:
+                http://www.keeptalkinggame.com/
+            </Grid>
+        </Grid>
     )
 }
 
